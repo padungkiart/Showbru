a5835dc1f1d3b77130024f89673fbc1a709b9a71
padungkiart <padungkiart009@gmail.com>
2018-04-26 11:09:19 +0700

    Finish 26 Apil 2018

--- app/src/main/java/com/example/android/showbru/fragmaent/RegisterFragment.java
+++ app/src/main/java/com/example/android/showbru/fragmaent/RegisterFragment.java
@@ -6,28 +6,80 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.android.showbru.MainActivity;
 import com.example.android.showbru.R;
+import com.example.android.showbru.utility.MyAlert;
 
-public class RegisterFragment extends Fragment{
+public class RegisterFragment extends Fragment {
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-    //    Create Toolbar
+        //    Create Toolbar
         createToolbar();
 
 
-        // Main Method
+    } // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemupload) {
+
+            uploadValueToServer();
+
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadValueToServer() {
+
+//        get value from EditText
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+//        Change Data Type From EditText to String
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+//        Check Space
+        if (nameString.isEmpty() || userString.isEmpty()|| passwordString.isEmpty()) {
+//             Have Space
+
+            MyAlert myAlert =new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space","Please Fill All Blank");
+
+        } else {
+//            No Space
+
+
+        }
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
     }
 
     private void createToolbar() {
-        Toolbar toolbar =getActivity().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
 
 //        setup Title
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Regiater");
@@ -44,12 +96,16 @@ public class RegisterFragment extends Fragment{
 
             }
         });
+
+        setHasOptionsMenu(true);
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_register,container,false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
     }
 }
